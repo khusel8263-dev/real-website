b8045cdfe8a29154f549041da26265a4289fceb7
Ankhbayar <ankhbayar.sukhee@gmail.com>
2020-08-28 15:07:08 +0800

    contact form -d fatal error ugsun uyed ilgeeh uildel amjiltgui bolson talaar message haruuldag bolgov. Ilgeeh tovch darahad loader haruuldag bolgov.

--- resources/assets/js/components/ContactFormComponent.vue
+++ resources/assets/js/components/ContactFormComponent.vue
@@ -1,8 +1,16 @@
 <template>
+    <div class="vld-parent">
+        <loading :active.sync="isLoading" 
+        :can-cancel="true"        
+        :is-full-page="fullPage">
+        </loading>
 
     <form @submit.prevent="submit">        
         <div v-if="success" class="alert alert-success mt-3">
             Таны захиа амжилттай илгээгдлээ!
+        </div>
+        <div v-if="errors && errors.fatal" class="alert alert-warning mt-3">
+            Захиа илгээх үйлдэл амжилтгүй боллоо. Та сайтын админтай холбогдоно уу!
         </div>
         <input v-model="contact.name" v-bind:class="{'required-error': errors && errors.name}" type="text" name="name" id="name" placeholder="Таны нэр*" class="input-bg">
                 
@@ -14,30 +22,47 @@
         
         <button id="contact-us-button" type="submit" class="btn btn-small border-radius-4 btn-black">Илгээх</button>
     </form>
-
+    </div>
 </template>
 
 <script>
+    // Import component
+    import Loading from 'vue-loading-overlay';
+    // Import stylesheet
+    import 'vue-loading-overlay/dist/vue-loading.css';
+
     export default {
         data() {
             return {
+                isLoading: false,
+                fullPage: true,
+
                 contact: {},
                 success: false,
                 errors: {},
             }
         },
+        components: {
+            Loading
+        },
         methods: {
             submit() {             
+                this.isLoading = true;
                 this.success = false;
                 this.errors = {};
                 axios.post('/contact', this.contact)
                 .then(response => {
                     this.contact = {};
                     this.success = true;
+                    this.isLoading = false;
                 })
                 .catch(error => {
+                    this.isLoading = false;
                     if (error.response.status === 422) {
-                    this.errors = error.response.data.errors || {};
+                        this.errors = error.response.data.errors || {};
+                    }
+                    else {
+                        this.errors = {fatal: error.response.status};                        
                     }
                 });
             },
